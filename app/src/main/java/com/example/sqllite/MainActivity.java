c3d1f9371823a519b493788af28d62176c59f9c1
pss407 <pss407@naver.com>
2020-06-17 21:18:25 +0900

    commit

--- app/src/main/java/com/example/sqllite/MainActivity.java
+++ app/src/main/java/com/example/sqllite/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
